7595a96a67cbca70646239fe66d1edc08495b810
Subasree-Vivekanandan <106468121+Subasree-Vivekanandan@users.noreply.github.com>
2023-03-12 21:40:44 +0530

    app.py fixed

--- deploy/app.py
+++ deploy/app.py
@@ -12,17 +12,57 @@ import pickle as pkl
 
 
 app = Flask(__name__)
-model = pkl.load(open('knn_eeg_model.pkl','rb'))
+knn_model = pkl.load(open('knn_eeg_model.pkl','rb'))
+svm_model = pkl.load(open('svm_eeg_model.pkl','rb'))
+lr_model = pkl.load(open('log_reg_model.pkl','rb'))
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
-@app.route('/predict',methods=['POST'])
-def predict():
+@app.route('/svm')
+def svm_page():
+    return render_template('svm.html')
+
+@app.route('/predictsvm',methods=['POST'])
+def predict_svm():
+    svmf = [float(x) for x in request.form.values()]
+    svm_final = [np.array(svmf)]
+    svm_prediction = svm_model.predict(svm_final)
+    
+    if (svm_prediction==0.0):
+        svm_output = 'Not Confused'
+    else:
+        svm_output = 'Confused'
+    
+    return  render_template('predict.html',prediction_text='Student is {}'.format(svm_output))
+
+@app.route('/logreg')
+def logreg_page():
+    return render_template('logreg.html')
+
+@app.route('/lrpredict',methods=['POST'])
+def predict_lr():
+    lr_features =  [float(x) for x in request.form.values()]
+    lr_final = [np.array(lr_features)]
+    lr_prediction = lr_model.predict(lr_final)
+    
+    if(lr_prediction==0.0):
+        lr_output = 'Not Confused'
+    else:
+        lr_output = 'Confused'
+    
+    return render_template('predict.html',prediction_text='Student is {}'.format(lr_output))
+    
+@app.route('/knn')
+def knn_page():
+    return render_template('knn.html')
+
+@app.route('/knnpredict',methods=['POST'])
+def predict_knn():
     features = [float(x) for x in request.form.values()]
     final_features = [np.array(features)]
-    prediction = model.predict(final_features)
+    prediction = knn_model.predict(final_features)
     
     if(prediction==0.0):
         output = 'Not Confused'
@@ -30,10 +70,11 @@ def predict():
         output = 'Confused'
     
     
-    return render_template('index.html',prediction_text='Student is {}'.format(output))
-
+    return render_template('predict.html',prediction_text='Student is {}'.format(output))
 
 if __name__ == "__main__":
-    app.run(debug=False,host='0.0.0.0')    
+    app.run(debug=True)    
         
+    
+    
     
